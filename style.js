f091da01b1d34cbabb03c5e7ddcf11ef33e38eff
Ahmad B <ahmadbello2011@gmail.com>
2022-12-13 10:32:44 -0800

    array work

---
+++ style.js
@@ -0,0 +1,32 @@
+let users=[
+   {name:'Tayo', age:129, address:'Osogbo'},
+   {name:'Sade', age:12, address:'Ondo'},
+   {name:'Emman', age:1200, address:'Kwara'},
+   {name:'Funke', age:200, address:'Akure'},
+   {name:'Yemi', age:4000, address:'Lagos'},
+   {name:'Kemi', age:100, address:'Abuja'},
+]
+document.getElementById('name0').innerHTML = (users[0].name)
+document.getElementById('age0').innerHTML = (users[0].age)
+document.getElementById('address0').innerHTML = (users[0].address)
+
+document.getElementById('name1').innerHTML = (users[1].name)
+document.getElementById('age1').innerHTML = (users[1].age)
+document.getElementById('address1').innerHTML = (users[1].address)
+
+document.getElementById('name2').innerHTML = (users[2].name)
+document.getElementById('age2').innerHTML = (users[2].age)
+document.getElementById('address2').innerHTML = (users[2].address)
+
+document.getElementById('name3').innerHTML = (users[3].name)
+document.getElementById('age3').innerHTML = (users[3].age)
+document.getElementById('address3').innerHTML = (users[3].address)
+
+document.getElementById('name4').innerHTML = (users[4].name)
+document.getElementById('age4').innerHTML = (users[4].age)
+document.getElementById('address4').innerHTML = (users[4].address)
+
+document.getElementById('name5').innerHTML = (users[5].name)
+document.getElementById('age5').innerHTML = (users[5].age)
+document.getElementById('address5').innerHTML = (users[5].address)
+
